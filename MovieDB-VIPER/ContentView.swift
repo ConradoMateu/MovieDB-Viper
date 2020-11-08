7837923fe034414a21316fca621e8103024dd09c
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2020-11-08 20:11:43 +0100

    Initial Commit

---
+++ MovieDB-VIPER/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDB-VIPER
+//
+//  Created by Emgy on 08/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieDB-VIPER/MovieDB_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB_VIPERApp.swift
+//  MovieDB-VIPER
+//
+//  Created by Emgy on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MovieDB_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
